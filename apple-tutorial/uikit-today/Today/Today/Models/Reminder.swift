ef6c31b53dd4292f20696b64ccf7e200c0b819cc
0tak2 <0tak2.code@gmail.com>
2024-12-18 13:15:41 +0900

    [apple-tutorial/uikit-today] feat: 신규 프로젝트 준비

---
+++ apple-tutorial/uikit-today/Today/Today/Models/Reminder.swift
@@ -0,0 +1,8 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by 임영택 on 12/18/24.
+//
+
+import Foundation
---
+++ apple-tutorial/uikit-today/Today/TodayTests/TodayTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodayTests.swift
+//  TodayTests
+//
+//  Created by 임영택 on 12/18/24.
+//
+
+import Testing
+@testable import Today
+
+struct TodayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
