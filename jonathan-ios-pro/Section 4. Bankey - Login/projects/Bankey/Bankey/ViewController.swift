d98f98ddd93c511502765e693d7061d9f149d8c7
0tak2 <0tak2.code@gmail.com>
2025-01-17 23:51:54 +0900

    [jonathan-io-pro] 4-14 스토리보드를 사용하지 않는 iOS 프로젝트 만들기

---
+++ jonathan-ios-pro/Section 4. Bankey - Login/projects/Bankey/Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by 임영택 on 1/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ jonathan-ios-pro/Section 4. Bankey - Login/projects/Bankey/BankeyTests/BankeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankeyTests.swift
+//  BankeyTests
+//
+//  Created by 임영택 on 1/17/25.
+//
+
+import Testing
+@testable import Bankey
+
+struct BankeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
