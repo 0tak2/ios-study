fa63536b4721a7872ab8c582ea3ed739764009b6
0tak2 <0tak2.code@gmail.com>
2025-03-16 22:07:53 +0900

    [fc-ios-clone-apps/voiceMemo] feat: 날짜 형식

--- fc-ios-clone-apps/voiceMemo/voiceMemo/Core/Extension/Date+Extensions.swift
+++ fc-ios-clone-apps/voiceMemo/voiceMemo/Core/Extension/Date+Extensions.swift
@@ -6,5 +6,32 @@
 import Foundation
 
 extension Date {
+  var formattedTime: String {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "ko_KR")
+    formatter.dateFormat = "a hh:mm"
+    return formatter.string(from: self)
+  }
   
+  var formattedDate: String {
+    let now = Date()
+    let calendar = Calendar.current
+    
+    let startTimeOfCurrentDate = calendar.startOfDay(for: now)
+    let startTimeOfSelf = calendar.startOfDay(for: self)
+    let numOfDaysDifference = calendar.dateComponents([.day], from: startTimeOfCurrentDate, to: startTimeOfSelf).day
+    
+    if let numOfDaysDifference = numOfDaysDifference {
+      if numOfDaysDifference == 0 {
+        return "오늘"
+      }
+      
+      let formatter = DateFormatter()
+      formatter.locale = Locale(identifier: "ko_KR")
+      formatter.dateFormat = "M월 d일 E요일"
+      return formatter.string(from: self)
+    }
+    
+    return "" // Never reached
+  }
 }
