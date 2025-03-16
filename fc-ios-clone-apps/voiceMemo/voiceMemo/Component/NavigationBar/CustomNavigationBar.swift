dc06c3485fcba89d9906f93e95d8c608caa17eea
0tak2 <0tak2.code@gmail.com>
2025-03-16 22:07:39 +0900

    [fc-ios-clone-apps/voiceMemo] feat: 네비게이션 바

--- fc-ios-clone-apps/voiceMemo/voiceMemo/Component/NavigationBar/CustomNavigationBar.swift
+++ fc-ios-clone-apps/voiceMemo/voiceMemo/Component/NavigationBar/CustomNavigationBar.swift
@@ -6,8 +6,49 @@
 import SwiftUI
 
 struct CustomNavigationBar: View {
+  let isDisplayLeftButton: Bool
+  let isDisplayRightButton: Bool
+  let leftButtonAction: () -> Void
+  let rightButtonAction: () -> Void
+  let rightButtonType: NavigationBtnType
+  
+  init(
+    isDisplayLeftButton: Bool = true,
+    isDisplayRightButton: Bool = true,
+    leftButtonAction: @escaping () -> Void = { },
+    rightButtonAction: @escaping () -> Void = { },
+    rightButtonType: NavigationBtnType = .edit
+  ) {
+    self.isDisplayLeftButton = isDisplayLeftButton
+    self.isDisplayRightButton = isDisplayRightButton
+    self.leftButtonAction = leftButtonAction
+    self.rightButtonAction = rightButtonAction
+    self.rightButtonType = rightButtonType
+  }
+  
   var body: some View {
-    Text("CustomNavigationBar")
+    HStack {
+      if isDisplayLeftButton {
+        Button(action: leftButtonAction) {
+          Image("leftArrow")
+        }
+      }
+      
+      Spacer()
+      
+      if isDisplayRightButton {
+        Button(action: rightButtonAction) {
+          if rightButtonType == .close {
+            Image("close")
+          } else {
+            Text(rightButtonType.rawValue)
+              .foregroundStyle(.black)
+          }
+        }
+      }
+    }
+    .padding(.horizontal, 20)
+    .frame(height: 20)
   }
 }
 
