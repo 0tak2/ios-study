ad426beb932e0463b760a4909e8acfe6a6224114
0tak2 <0tak2.code@gmail.com>
2025-06-27 14:30:33 +0900

    [ARKitPractice2] feat: init project

---
+++ self-study/ARKitPractice2/ARKitPractice2Tests/ARKitPractice2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitPractice2Tests.swift
+//  ARKitPractice2Tests
+//
+//  Created by 임영택 on 6/27/25.
+//
+
+import Testing
+@testable import ARKitPractice2
+
+struct ARKitPractice2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
