dffb03c9f726528e022c54668b51a5b00cd977be
0tak2 <0tak2.code@gmail.com>
2025-07-03 10:54:12 +0900

    [ObjectTrackingGA]

---
+++ self-study/ObjectTrackingGA/ObjectTrackingGA/ObjectTrackingGAApp.swift
@@ -0,0 +1,34 @@
+//
+//  ObjectTrackingGAApp.swift
+//  ObjectTrackingGA
+//
+//  Created by 임영택 on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectTrackingGAApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
