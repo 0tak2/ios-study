e1c68742bf39ea79e39139c64a8f260f67d399a8
0tak2 <0tak2.code@gmail.com>
2025-07-15 14:53:39 +0900

    [SceneUnderstandingPractice] feat: 게임 상태를 관리하는 GameManager 구현

---
+++ self-study/SceneUnderstandingPractice/SceneUnderstandingPractice/AR/GameManager.swift
@@ -0,0 +1,62 @@
+//
+//  GameManager.swift
+//  SceneUnderstandingPractice
+//
+//  Created by 임영택 on 7/15/25.
+//
+
+import Foundation
+import RealityKit
+
+class GameManager {
+    static let instance = GameManager()
+    
+    private(set) var currentMode: GameMode = .initialized
+    private(set) var totalCardCount: Int?
+    private(set) var attachedCardsCount = 0
+    
+    private init() { }
+    
+    func config(totalCardCount: Int) throws {
+        guard currentMode == .initialized else {
+            throw GameManagerError.gameAlreadyConfigured
+        }
+        
+        self.totalCardCount = totalCardCount
+        self.currentMode = .idle
+    }
+    
+    func cardsAttached(count: Int = 1) {
+        print("\(count) of Cards attached")
+        
+        attachedCardsCount += count
+        
+        currentMode = attachedCardsCount == totalCardCount! ? .ready : .idle
+    }
+    
+    func cardDetached(count: Int = 1) {
+        print("\(count) of Cards detached")
+        
+        attachedCardsCount -= count
+        
+        currentMode = attachedCardsCount == totalCardCount! ? .ready : .idle
+    }
+    
+    enum GameMode {
+        case initialized // 처음 초기화됨
+        case idle // 게임 설정이 완료됨
+        case ready // 모든 카드를 붙여서 게임 준비가 완료됨
+        case playing // 게임이 시작됨
+    }
+    
+    enum GameManagerError: Error, LocalizedError {
+        case gameAlreadyConfigured
+        
+        var errorDescription: String? {
+            switch self {
+            case .gameAlreadyConfigured:
+                return "Game is already configured."
+            }
+        }
+    }
+}
