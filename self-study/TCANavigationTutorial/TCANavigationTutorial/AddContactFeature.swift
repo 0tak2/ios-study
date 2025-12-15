b114337d770ec810e6c46c32793a962303f1ab2d
0tak2 <0tak2.code@gmail.com>
2025-12-15 15:16:19 +0900

    [TCANavigationTutorial] 자식-부모 통신에 Delegate 패턴 적용

--- self-study/TCANavigationTutorial/TCANavigationTutorial/AddContactFeature.swift
+++ self-study/TCANavigationTutorial/TCANavigationTutorial/AddContactFeature.swift
@@ -14,19 +14,33 @@ struct AddContactFeature {
     var contact: Contact
   }
   
+  @Dependency(\.dismiss) var dismiss
+  
   enum Action {
     case cancelButtonTapped
+    case delegate(Delegate)
     case saveButtonTapped
     case setName(String)
+    
+    enum Delegate: Equatable {
+      case saveContact(Contact)
+    }
   }
   
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
       case .cancelButtonTapped:
+        return .run { _ in
+          await dismiss()
+        }
+      case .delegate:
         return .none
       case .saveButtonTapped:
-        return .none
+        return .run { [contact = state.contact] send in
+          await send(.delegate(.saveContact(contact)))
+          await dismiss()
+        }
       case .setName(let name):
         state.contact.name = name
         return .none
