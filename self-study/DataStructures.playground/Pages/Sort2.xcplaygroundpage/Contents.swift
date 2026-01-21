e27b50a5b9745c4d9466363f29e60744a2d2fcba
0tak2 <0tak2.code@gmail.com>
2026-01-22 08:47:34 +0900

    [DataStructures] fix: Quick Sort

--- self-study/DataStructures.playground/Pages/Sort2.xcplaygroundpage/Contents.swift
+++ self-study/DataStructures.playground/Pages/Sort2.xcplaygroundpage/Contents.swift
@@ -1,17 +1,19 @@
 //: [Previous](@previous)
 
+// MARK: - Quick Sort
+
 func quickSortPartition(array: inout [Int], start: Int, end: Int) -> Int {
-    let pivot = array[start]
+    let pivot = array[start] // 첫 값을 피벗으로 사용
     
     var left = start + 1
     var right = end
     
     while left <= right {
         while left <= end && array[left] < pivot {
-            left += 1 // 왼쪽에서 피벗보다 작은 값이 나오면 한 칸 이동
+            left += 1 // 왼쪽에서 피벗보다 큰 값이 나올때 까지 앞으로 이동
         }
         while right > start && array[right] > pivot {
-            right -= 1 // 오른쪽에서 피벗보다 큰 값이 나오면 한 칸 이동
+            right -= 1 // 오른쪽에서 피벗보다 작은 값이 나올 때까지 뒤로 이동
         }
         
         if left < right { // 한 번 더 검사
@@ -20,13 +22,10 @@ func quickSortPartition(array: inout [Int], start: Int, end: Int) -> Int {
             let temp = array[left]
             array[left] = array[right]
             array[right] = temp
-            
-            left += 1
-            right -= 1
         }
     }
     
-    // 피벗과 right 스왑
+    // 피벗 자리와 지금 right 자리를 바꿔서 피벗을 가운데로 옮기고 그 인덱스를 반환 (새 파티션)
     let temp = array[start]
     array[start] = array[right]
     array[right] = temp
@@ -34,18 +33,21 @@ func quickSortPartition(array: inout [Int], start: Int, end: Int) -> Int {
     return right
 }
 
-func quickSort(of array: inout [Int], start: Int = 0, end: Int = -1) {
-    let end = end == -1 ? array.count - 1 : end
+func quickSort(of array: inout [Int], start: Int, end: Int) {
     if start < end {
         let pivot = quickSortPartition(array: &array, start: start, end: end)
-        
         quickSort(of: &array, start: start, end: pivot - 1)
         quickSort(of: &array, start: pivot + 1, end: end)
     }
 }
 
+func quickSort(of array: inout [Int]) {
+  quickSort(of: &array, start: 0, end: array.count - 1)
+}
+
 var arr = [1, 5, 3, 2, 4]
 quickSort(of: &arr)
 print(arr)
+
 
 //: [Next](@next)
